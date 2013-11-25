ac3dfd2649563d2fb1f48ae71cdda113fd908a23
Tim White <timothy.white@TWhite-C30.hps.com>
2013-11-25 16:19:36 -0500

    Added Bursting

--- src/com/game/Enemy.java
+++ src/com/game/Enemy.java
@@ -23,6 +23,9 @@ public class Enemy {
 	private boolean ready;
 	private boolean dead;
 	
+	private boolean hit;
+	private long hitTimer;
+	
 	public Enemy(int type, int rank){
 		this.type = type;
 		this.rank = rank;
@@ -34,6 +37,21 @@ public class Enemy {
 			speed = 2;
 			r=5;
 			health =1;
+			}
+			if (rank == 2){
+				speed = 2;
+				r=10;
+				health =2;	
+			}
+			if (rank == 3){
+				speed = 2;
+				r=20;
+				health =3;	
+			}
+			if (rank == 4){
+				speed = 2;
+				r=30;
+				health =4;	
 			}
 		}
 		//stonger, faster default
@@ -66,6 +84,9 @@ public class Enemy {
 		ready = false;
 		dead = false;
 		
+		hit = false;
+		hitTimer = 0;
+		
 	}
 	
 	public double getx(){
@@ -83,11 +104,38 @@ public class Enemy {
 	public boolean isDead(){
 		return dead;
 	}
+	
+	public void explode(){
+		if(rank>1){
+			int amount = 0;
+				if(type ==1){
+					amount = 3;
+				}
+				for (int i = 0;i<amount;i++){
+					Enemy e = new Enemy(getType(),getRank() -1);
+					e.x = this.x;
+					e.y = this.y;
+					double angle  = 0;
+					if(!ready){
+						angle = Math.random() * 140 + 20;
+					}
+					else{
+						angle = Math.random() * 360;
+					}
+					e.rad  = Math.toRadians(angle);
+					GamePanel.enemies.add(e);
+				}
+		}
+	}
+	
+	
 	public void hit(){
 		health--;
 		if(health <=0){
 			dead = true;
 		}
+		hit = true;
+		hitTimer = System.nanoTime();
 	}
 	
 	public void update (){
@@ -105,10 +153,27 @@ public class Enemy {
 		if(x>GamePanel.WIDTH-r && dx >0)dx = -dx;
 		if(y>GamePanel.HEIGHT-r && dy >0)dy = -dy;
 		
+		if(hit){
+			long elapsed = (System.nanoTime() - hitTimer) / 1000000;
+			if(elapsed > 50){
+				hit = false;
+				hitTimer = 0;
+			}
+		}
+		
+		
 	}
 	
 	public void draw(Graphics2D g){
-		
+		if(hit){
+			g.setColor(Color.WHITE);
+			g.fillOval((int) (x-r), (int) (y-r), 2*r, 2*r);
+			
+			g.setStroke(new BasicStroke(3));
+			g.setColor(Color.WHITE.darker());
+			g.drawOval((int) (x-r), (int) (y-r), 2*r, 2*r);
+			g.setStroke(new BasicStroke(1));
+		}else{
 		g.setColor(color1);
 		g.fillOval((int) (x-r), (int) (y-r), 2*r, 2*r);
 		
@@ -116,6 +181,7 @@ public class Enemy {
 		g.setColor(color1.darker());
 		g.drawOval((int) (x-r), (int) (y-r), 2*r, 2*r);
 		g.setStroke(new BasicStroke(1));
+		}
 		
 	}
 	
--- src/com/game/GamePanel.java
+++ src/com/game/GamePanel.java
@@ -189,10 +189,10 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 					if(rand < 0.001) powerups.add(new PowerUp(1,e.getx(),e.gety()));
 					else if(rand < 0.120) powerups.add(new PowerUp(2,e.getx(),e.gety()));
 					else if(rand < 0.020) powerups.add(new PowerUp(3,e.getx(),e.gety()));
-					else  powerups.add(new PowerUp(2,e.getx(),e.gety()));
 					player.addScore((e.getType() + e.getRank())*5);
 					enemies.remove(j);
 					j--;
+					e.explode();
 				}
 		}
 		
@@ -315,17 +315,13 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 			for(int i = 0; i<8;i++){
 				enemies.add(new Enemy(1,1));
 			}
+			enemies.add(new Enemy(1,2));
+			enemies.add(new Enemy(1,2));
 		}
 		if(waveNumber == 3){
-			for(int i = 0; i<4;i++){
-				enemies.add(new Enemy(1,1));
-			}
-			for(int i = 0; i<4;i++){
-				enemies.add(new Enemy(2,1));
-			}
-			for(int i = 0; i<4;i++){
-				enemies.add(new Enemy(3,1));
-			}
+			enemies.add(new Enemy(1,3));
+			enemies.add(new Enemy(1,3));
+			enemies.add(new Enemy(1,4));
 		}
 	}
 	
